aabfabd8edb3bbc16b3793ebeb249b1488807476
keziakl <keziakl@stanford.edu>
2020-09-19 16:40:39 -0500

    finished text analysis functionality

--- analysis/texta.py
+++ analysis/texta.py
@@ -10,12 +10,17 @@ url_start = "https://www.gutenberg.org/files/"
 bookid = "768"
 full_url = url_start + bookid + "/" + bookid + ".txt"
 
+#create  Text object
 def grab_text_and_process(full_url):
     response = request.urlopen(full_url)
     raw = response.read().decode('utf8')
     tokens = nltk.word_tokenize(raw)
     text = nltk.Text(tokens)
     return text
+
+#create pos tagger
+def pos_tagged(text):
+    return nltk.pos_tag(text)
 
 #sentiment analysis
 nltk.download('vader_lexicon')
@@ -24,6 +29,14 @@ def sentan_scores(raw):
     scores = sid.polarity_scores(raw)
     #format is dict containing: {'neg': 0.072, 'neu': 0.859, 'pos': 0.069, 'compound': -0.8426}
     return scores
+
+def sent(scores):
+    if scores["neg"] - scores["pos"] > 0:
+        return "neg"
+    elif scores["neg"] - scores["pos"] < 0:
+        return "pos"
+    else:
+        return "neut"
 
 #similar words (words used in the same context -> ex monstrous)
 def similar_words(text, w="monstrous"):
@@ -37,10 +50,10 @@ def num_words_total(text):
 def lexical_diversity(text):
     len(set(text)) / len(text)
 
-#finds common words (hopefully excluding stop words, words like "said", etc.)
-def common_words(text):
+#finds most common words
+def common_words_50(text):
     fdist = FreqDist(text)
-    return sorted(w for w in set(text) if fdist[w] >= 10 and fdist[w] <=35)
+    return fdist.most_common(50)
 
 # random pseudo common word from set
 def random_common_word(cw):
@@ -48,5 +61,50 @@ def random_common_word(cw):
     cw.remove("Chapter")
     return random.choice(cw)
 
+#returns all of some part of speech "tag" in text
+#tags are: NN (noun), RB (adverb), VB (verb), JJ (ajective)
+def only_pos(postag, tag="NN"):
+    return [i for i, j in postag if tag in j]
+
+def top_num(w, num="10"):
+    return w[:num]
+
+def piechart(postag, numwords):
+    dic = {}
+    count = 0
+    pn = len(only_pos(postag, "NN"))/numwords
+    dic["NN"] = pn
+    count += pn
+    pn = len(only_pos(postag, "JJ"))/numwords
+    dic["ADJ"] = pn
+    count += pn
+    pn = len(only_pos(postag, "VB"))/numwords
+    dic["VB"] = pn
+    count += pn
+    pn = len(only_pos(postag, "RB"))/numwords
+    dic["ADV"] = pn
+    count += pn
+    dic["OTHER"] = count
+    return dic
+
 text = grab_text_and_process(full_url)
-print(common_words(text))
+postag = pos_tagged(text)
+num = num_words_total(text)
+#print(top_10(only_pos(postag)))
+print(common_words_50(only_pos(postag)))
+print(piechart(postag, num))
+
+'''
+possibliities with these functions:
+- return the most commonly used words of a pos category by the author (list)
+        - top from all words (max 50, includes punctuation)
+        -top num most common nouns, adverbs, adjectives (list)
+        - display a random common word (from a pos category or not) (string)
+- display the sentiment of the entire book (dict of sentiments)
+        - get if "pos", "neg" or "neut" (string)
+- display words used in the same context as a certain query word (list)
+- display the lexical diversity of the book (decimal)
+- display num words in book (int)
+- create piechart with 5 categories (noun, adj, adv, vb, other)
+        -returns dict of "NN", "ADJ", "ADV", "VB", "OTHER" with corresponding decimals (percentages)
+'''
